22f95f39bc6fc78ead90f592bbc9361a38f002c8
Ifeanyi Madu <supermadu7@gmail.com>
2025-07-13 21:34:05 +0100

    update sidebar

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -17,6 +17,36 @@
                             </a>
                         </li>
                         <!-- End::slide -->
+
+                        <!-- Start::slide__category -->
+                        <li class="slide__category"><span class="category-name">Travel Management</span></li>
+                        <!-- End::slide__category -->
+
+                        <!-- Start::slide -->
+                        <li class="slide">
+                            <a href="travel-requests.php" class="side-menu__item">
+                                <span class="shape1"></span>
+                                <span class="shape2"></span>
+                                <i class="ti-briefcase side-menu__icon"></i>
+                                <span class="side-menu__label">Travel Requests</span>
+                            </a>
+                        </li>
+                        <!-- End::slide -->
+
+                        <!-- Start::slide__category -->
+                        <li class="slide__category"><span class="category-name">Administration</span></li>
+                        <!-- End::slide__category -->
+
+                        <!-- Start::slide -->
+                        <li class="slide">
+                            <a href="user-management.php" class="side-menu__item">
+                                <span class="shape1"></span>
+                                <span class="shape2"></span>
+                                <i class="ti-user side-menu__icon"></i>
+                                <span class="side-menu__label">User Management</span>
+                            </a>
+                        </li>
+                        <!-- End::slide -->
  
                     </ul>
                    
